5c4810b89ff0a8ce28c0c41ebd7457a6d6804baa
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-01 14:37:52 -0300

    added karting routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
-
+import kartingRoutes from "./routes/karting.routes";
 const app = express();
-
+app.use("/api/kartings", kartingRoutes);
 // Middleware para recibir JSON
 app.use(express.json());
 
