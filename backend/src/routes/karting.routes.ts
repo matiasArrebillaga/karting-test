bc51c248c9dd080f8da348b28461cefc36b75979
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-03 12:15:56 -0300

    added routes for karting

--- backend/src/routes/karting.routes.ts
+++ backend/src/routes/karting.routes.ts
@@ -1,5 +1,11 @@
-import { Router } from "express";
+import {Router} from "express";
+import kartingController from "../controllers/karting.controller"
 
 const router = Router();
 
+router.get("/", kartingController.getAll);
+router.get("/", kartingController.getById);
+router.post("/", kartingController.create);
+router.put("/", kartingController.update);
+router.put("/", kartingController.delete);
 export default router;
