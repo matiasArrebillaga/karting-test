d6ae9cf11f112805841a8900ec0da395bef40567
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-04 13:02:43 -0300

    added user service

---
+++ backend/src/services/user.services.ts
@@ -0,0 +1,21 @@
+import User from "../models/user.model";
+import { IUser } from "../interfaces/user.interface";
+
+class UserService {
+    async getAllUsers(){
+        return await User.find();
+    }
+    async getUserbyId (id: string){
+        return await User.findById(id);
+    }
+    async createUser (data: IUser) {
+        return await User.create(data);
+    }
+    async updateUser(id:string, data:Partial<IUser>){
+        return await User.findByIdAndUpdate(id, data,{new:true});
+    }
+    async deleteUser (id:string){
+        return await User.findByIdAndUpdate(id)
+    }
+}
+export default new UserService();
