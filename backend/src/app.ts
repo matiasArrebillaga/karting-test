014bedd73efaf76f6880bdffded131081f6ca409
matiasArrebillaga <matiasarrebillaga@gmail.com>
2026-08-14 16:24:47 -0300

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,11 @@ import express from "express";
 import kartingRoutes from "./routes/karting.routes";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/kartings", kartingRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
